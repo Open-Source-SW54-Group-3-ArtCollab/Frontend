abbe62ad7bcb700d0d63be74387dbbfb9c173b9b
Mathias Jave Diaz <u202212721@upc.edu.pe>
2024-04-24 15:46:17 -0500

    feat(footer): added footer content

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavbarContentComponent} from "./public/components/navbar-content/navbar-content.component";
+import {FooterContentComponent} from "./public/components/footer-content/footer-content.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarContentComponent],
+  imports: [RouterOutlet, NavbarContentComponent, FooterContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- frontend/src/app/public/components/footer-content/footer-content.component.ts
+++ frontend/src/app/public/components/footer-content/footer-content.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
-  selector: 'app-footer-content',
+  selector: 'footer-content',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIcon
+  ],
   templateUrl: './footer-content.component.html',
   styleUrl: './footer-content.component.css'
 })
