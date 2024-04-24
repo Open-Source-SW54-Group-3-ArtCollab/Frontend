eab3f238d7333730bcdd55175931acd5b739c3e7
Gustavo Huilca <u202213983@upc.edu.pe>
2024-04-24 13:53:25 -0500

    feat: Added sidenav to responsive navbar

--- frontend/src/app/public/components/navbar-content/navbar-content.component.ts
+++ frontend/src/app/public/components/navbar-content/navbar-content.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component, ViewChild} from '@angular/core';
 import {MatToolbar} from "@angular/material/toolbar";
 import {MatIcon} from "@angular/material/icon";
 import {MatIconButton} from "@angular/material/button";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatListItem, MatNavList} from "@angular/material/list";
+import {MediaMatcher} from "@angular/cdk/layout";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'navbar-content',
@@ -9,11 +13,28 @@ import {MatIconButton} from "@angular/material/button";
   imports: [
     MatToolbar,
     MatIcon,
-    MatIconButton
+    MatIconButton,
+    MatSidenavModule,
+    MatNavList,
+    MatListItem,
+    RouterLink
   ],
   templateUrl: './navbar-content.component.html',
   styleUrl: './navbar-content.component.css'
 })
 export class NavbarContentComponent {
+  mobileQuery: MediaQueryList;
+
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 1024px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
 
 }
