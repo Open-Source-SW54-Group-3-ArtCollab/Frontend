fd01714b883f9537d351755b8c7942d6ffedc74e
CamiAm404 <u202114248@upc.edu.pe>
2024-04-24 17:07:15 -0500

    feat: Added font awesome icons
    Added font awesome icons into footer

--- frontend/src/app/public/components/footer-content/footer-content.component.ts
+++ frontend/src/app/public/components/footer-content/footer-content.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import {MatIcon} from "@angular/material/icon";
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faXTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'footer-content',
   standalone: true,
   imports: [
-    MatIcon
+    MatIcon,
+    FontAwesomeModule
   ],
   templateUrl: './footer-content.component.html',
   styleUrl: './footer-content.component.css'
 })
 export class FooterContentComponent {
-
+  faXTwitter = faXTwitter;
+  faInstagram = faInstagram;
 }
