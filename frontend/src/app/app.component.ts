9be0cbea461f69a6c2cc673af853ea297caea92c
Gustavo Huilca <u202213983@upc.edu.pe>
2024-04-26 12:50:11 -0500

    feat: add main-page banner

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavbarContentComponent} from "./public/components/navbar-content/navbar-content.component";
 import {FooterContentComponent} from "./public/components/footer-content/footer-content.component";
+import {
+  TheMainPageBannerComponent
+} from "./content/components/main-page/the-main-page-banner/the-main-page-banner.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarContentComponent, FooterContentComponent],
+  imports: [RouterOutlet, NavbarContentComponent, FooterContentComponent, TheMainPageBannerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- frontend/src/app/content/components/main-page/the-main-page-banner/the-main-page-banner.component.ts
+++ frontend/src/app/content/components/main-page/the-main-page-banner/the-main-page-banner.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild, AfterViewInit} from '@angular/core';
+import {GalleryModule, GalleryComponent, GalleryItem, ImageItem} from "ng-gallery";
 
 @Component({
   selector: 'app-the-main-page-banner',
   standalone: true,
-  imports: [],
+  imports: [
+    GalleryComponent
+  ],
   templateUrl: './the-main-page-banner.component.html',
   styleUrl: './the-main-page-banner.component.css'
 })
-export class TheMainPageBannerComponent {
+export class TheMainPageBannerComponent implements OnInit, AfterViewInit {
 
+  @ViewChild(GalleryComponent) gallery!: GalleryComponent;
+
+  ngOnInit() {
+
+  }
+
+  ngAfterViewInit() {
+    this.gallery.load([
+      new ImageItem({src: 'https://picsum.photos/id/237/200/300'}),
+      new ImageItem({src: 'https://picsum.photos/id/238/200/300'}),
+      new ImageItem({src: 'https://picsum.photos/id/239/200/300'}),
+      new ImageItem({src: 'https://picsum.photos/id/237/200/300'}),
+    ]);
+  }
 }
