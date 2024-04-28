81b69698187cd5e41b9343a16fc320973a02c5ab
Tato <italoluna23@gmail.com>
2024-04-28 11:14:50 -0500

    feat: added icons for social networks in writer profile

--- frontend/src/app/user/components/the-writer-profile/the-writer-profile.component.ts
+++ frontend/src/app/user/components/the-writer-profile/the-writer-profile.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import {MatCard, MatCardContent, MatCardImage} from "@angular/material/card";
 import {MatButton} from "@angular/material/button";
+import {MatList, MatListItem} from "@angular/material/list";
+import {faInstagram, faTiktok, faXTwitter, faYoutube} from "@fortawesome/free-brands-svg-icons";
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-the-writer-profile',
@@ -9,11 +13,19 @@ import {MatButton} from "@angular/material/button";
     MatCard,
     MatCardContent,
     MatCardImage,
-    MatButton
+    MatButton,
+    MatList,
+    MatListItem,
+    FaIconComponent,
+    MatIcon
   ],
   templateUrl: './the-writer-profile.component.html',
   styleUrl: './the-writer-profile.component.css'
 })
 export class TheWriterProfileComponent {
 
+  protected readonly faXTwitter = faXTwitter;
+  protected readonly faInstagram = faInstagram;
+  protected readonly faYoutube = faYoutube;
+  protected readonly faTiktok = faTiktok;
 }
