4b15cb131144a040d74354b3e15fb3d3def60d00
Tato <italoluna23@gmail.com>
2024-04-27 21:29:20 -0500

    feat: added images for artist use page

--- frontend/src/app/user/components/the-user-artist/the-user-artist.component.ts
+++ frontend/src/app/user/components/the-user-artist/the-user-artist.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {MatCard, MatCardTitle} from "@angular/material/card";
 
 @Component({
   selector: 'app-the-user-artist',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCard,
+    MatCardTitle
+  ],
   templateUrl: './the-user-artist.component.html',
   styleUrl: './the-user-artist.component.css'
 })
