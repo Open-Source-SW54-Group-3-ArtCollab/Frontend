288e752c5a18905c1afad3e9f255bd0241419572
CamiAm404 <u202114248@upc.edu.pe>
2024-04-28 17:18:42 -0500

    feat(book-publish-stepper): Added component

---
+++ frontend/src/app/content/components/publish/book-publish-stepper/book-publish-stepper.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import {FormBuilder, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatButtonModule} from '@angular/material/button';
+@Component({
+  selector: 'app-book-publish-stepper',
+  standalone: true,
+  imports: [MatButtonModule,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,],
+  templateUrl: './book-publish-stepper.component.html',
+  styleUrl: './book-publish-stepper.component.css'
+})
+export class BookPublishStepperComponent {
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+  isLinear = false;
+
+  constructor(private _formBuilder: FormBuilder) {}
+}
