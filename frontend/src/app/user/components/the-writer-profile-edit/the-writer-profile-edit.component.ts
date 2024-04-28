a90279ca93e8da446a484bcfe2823c22ee6dcb9d
Tato <italoluna23@gmail.com>
2024-04-28 13:15:16 -0500

    feat: added components to writer profile edit

---
+++ frontend/src/app/user/components/the-writer-profile-edit/the-writer-profile-edit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-the-writer-profile-edit',
+  standalone: true,
+  imports: [],
+  templateUrl: './the-writer-profile-edit.component.html',
+  styleUrl: './the-writer-profile-edit.component.css'
+})
+export class TheWriterProfileEditComponent {
+
+}
---
+++ frontend/src/app/user/pages/writer-profile-edit/writer-profile-edit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-writer-profile-edit',
+  standalone: true,
+  imports: [],
+  templateUrl: './writer-profile-edit.component.html',
+  styleUrl: './writer-profile-edit.component.css'
+})
+export class WriterProfileEditComponent {
+
+}
