6ff6cc8e2d647cc3cad8aaa54b1f1799c598f8ce
Tato <italoluna23@gmail.com>
2024-04-28 02:29:46 -0500

    feat: added image and buttons to write profile

--- frontend/src/app/user/components/the-writer-profile/the-writer-profile.component.ts
+++ frontend/src/app/user/components/the-writer-profile/the-writer-profile.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import {MatCard, MatCardContent, MatCardImage} from "@angular/material/card";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-the-writer-profile',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCard,
+    MatCardContent,
+    MatCardImage,
+    MatButton
+  ],
   templateUrl: './the-writer-profile.component.html',
   styleUrl: './the-writer-profile.component.css'
 })
