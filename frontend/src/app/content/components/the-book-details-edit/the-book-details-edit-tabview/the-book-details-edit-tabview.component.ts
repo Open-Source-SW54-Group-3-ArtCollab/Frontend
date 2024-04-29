e1fb32fea4c8197d3e204df9aed806984d0c3f9e
CamiAm404 <u202114248@upc.edu.pe>
2024-04-29 13:08:13 -0500

    feat(the-book-details-edit-tabview): Added first content
    Added components to "Detalles de la historia" section.

--- frontend/src/app/content/components/the-book-details-edit/the-book-details-edit-tabview/the-book-details-edit-tabview.component.ts
+++ frontend/src/app/content/components/the-book-details-edit/the-book-details-edit-tabview/the-book-details-edit-tabview.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import {MatTabsModule} from '@angular/material/tabs';
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {FormsModule} from "@angular/forms";
 @Component({
-  selector: 'app-the-book-details-edit-tabview',
+  selector: 'the-book-details-edit-tabview',
   standalone: true,
-  imports: [MatTabsModule],
+  imports: [MatTabsModule, MatFormField, MatInput, MatLabel, FormsModule],
   templateUrl: './the-book-details-edit-tabview.component.html',
   styleUrl: './the-book-details-edit-tabview.component.css'
 })
-export class TheBookDetailsEditTabviewComponent {}
+export class TheBookDetailsEditTabviewComponent {
+  value = 'Boulevard';
+}
