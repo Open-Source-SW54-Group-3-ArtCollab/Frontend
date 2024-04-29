cb26f6d87e9ad5e5d5d1ff05263f034c0a6c96b7
Gustavo Huilca <u202213983@upc.edu.pe>
2024-04-29 11:28:40 -0500

    feat: Linked user register with service

--- frontend/src/app/shared/service/users.service.ts
+++ frontend/src/app/shared/service/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {User} from "../model/user.entity";
 import {environment} from "../../../environtments/environment";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +11,12 @@ export class UsersService {
    baseUrl: string = environment.baseUrl;
   constructor(private http: HttpClient) { }
    getAll(){
-     return this.http.get(this.baseUrl);
+     return this.http.get(this.baseUrl+'/reader');
    }
    edit(id:any, name:any){
-     return this.http.patch(this.baseUrl +`/${id}`, {firstName:name} )
+     return this.http.patch(this.baseUrl+'/reader' +`/${id}`, {firstName:name} )
+   }
+   createUser(item:any):Observable<User> {
+     return this.http.post<User>(this.baseUrl+'/reader', JSON.stringify(item))
    }
 }
