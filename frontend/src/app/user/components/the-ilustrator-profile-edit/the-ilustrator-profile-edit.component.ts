9455ff59c8237bd601b2fc5bd6db5d4b1ec7001b
Tato <italoluna23@gmail.com>
2024-04-28 22:10:34 -0500

    feat: added photos of portafolios

--- frontend/src/app/user/components/the-ilustrator-profile-edit/the-ilustrator-profile-edit.component.ts
+++ frontend/src/app/user/components/the-ilustrator-profile-edit/the-ilustrator-profile-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import {MatIcon} from "@angular/material/icon";
-import {MatCard, MatCardContent} from "@angular/material/card";
+import {MatCard, MatCardContent, MatCardImage} from "@angular/material/card";
 import {FaIconComponent} from "@fortawesome/angular-fontawesome";
 import {faInstagram, faTiktok, faXTwitter} from "@fortawesome/free-brands-svg-icons";
 import {MatList, MatListItem} from "@angular/material/list";
@@ -14,7 +14,8 @@ import {MatList, MatListItem} from "@angular/material/list";
     MatCardContent,
     FaIconComponent,
     MatListItem,
-    MatList
+    MatList,
+    MatCardImage
   ],
   templateUrl: './the-ilustrator-profile-edit.component.html',
   styleUrl: './the-ilustrator-profile-edit.component.css'
