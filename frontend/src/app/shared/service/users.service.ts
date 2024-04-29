9487a8af9daa7ddae4979d58e4d72195dc3ca5d9
Gustavo Huilca <u202213983@upc.edu.pe>
2024-04-29 17:05:17 -0500

    feat: Add data to db.json

--- frontend/src/app/shared/service/users.service.ts
+++ frontend/src/app/shared/service/users.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {environment} from "../../environtments/environment";
+import {environment} from "../../../environtments/environment";
 import {User} from "../model/user.entity";
 
 @Injectable({
