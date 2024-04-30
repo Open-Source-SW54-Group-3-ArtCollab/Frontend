344eeb8018255e305179e3a110219d158a2b3f3c
Samira Alvarez <samirajetzabel@gmail.com>
2024-04-30 09:39:42 -0500

    feat (the-book-details): add iteration for data

--- frontend/src/app/shared/components/the-book-details/the-book-details.component.ts
+++ frontend/src/app/shared/components/the-book-details/the-book-details.component.ts
@@ -1,21 +1,23 @@
 import { Component } from '@angular/core';
 import {MatCardModule} from "@angular/material/card";
-import {faEye, faHeart, faLock} from "@fortawesome/free-solid-svg-icons";
+import {faEye, faHeart} from "@fortawesome/free-solid-svg-icons";
 import {FaIconComponent} from "@fortawesome/angular-fontawesome";
 import {MatButton} from "@angular/material/button";
+import {NgForOf, NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-the-book-details',
   standalone: true,
   imports: [MatCardModule,
     FaIconComponent,
-    MatButton],
+    MatButton,
+    NgForOf,
+    NgOptimizedImage],
   templateUrl: './the-book-details.component.html',
   styleUrl: './the-book-details.component.css'
 })
 export class TheBookDetailsComponent {
 
-  protected readonly faLock = faLock;
   protected readonly faHeart = faHeart;
   protected readonly faEye = faEye;
 
@@ -23,9 +25,14 @@ export class TheBookDetailsComponent {
     {cover:'https://m.media-amazon.com/images/I/81mUJVc1MkL._AC_UF1000,1000_QL80_DpWeblab_.jpg',
       title:'Boulevard',
       authors:'Flor M. Salvador / MMIvens',
-      date:'Actualizada abr. 02, 2024 10:11 AM',
+      date_publish:'Actualizada abr. 02, 2024 10:11 AM',
       views:'33 M',
-      likes:'15 M',}
+      likes:'15 M',},
+    {cover:'https://m.media-amazon.com/images/I/81mUJVc1MkL._AC_UF1000,1000_QL80_DpWeblab_.jpg',
+      title:'Boulevard',
+      authors:'Flor M. Salvador / MMIvens',
+      date_publish:'Actualizada abr. 02, 2024 10:11 AM',
+      views:'33 M',
+      likes:'15 M',},
   ]
-
 }
