e974b29c31883dc0bf318c9374c30fa2674c1387
JuanAlejandroCuadrosRodriguez <112289890+JuanAlejandroCuadrosRodriguez@users.noreply.github.com>
2024-05-01 14:48:46 -0500

    feat: Add the-artist-room in components

---
+++ frontend/src/app/collaboration/components/the-artist-room/the-artist-room.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {MatCard} from "@angular/material/card";
+import {MatIcon} from "@angular/material/icon";
+
+@Component({
+  selector: 'app-the-artist-room',
+  standalone: true,
+    imports: [
+        MatCard,
+        MatIcon
+    ],
+  templateUrl: './the-artist-room.component.html',
+  styleUrl: './the-artist-room.component.css'
+})
+export class TheArtistRoomComponent {
+  room = 'Room';
+  chat = 'Chat';
+  newchat = 'New Chat';
+  text = 'Text';
+  deleteroom = 'Delete Room';
+}
+
