df7b249020e2eeae98c26b2b443dbe5e4650d78a
Tato <italoluna23@gmail.com>
2024-04-30 22:34:06 -0500

    feat: added connected fake api

--- frontend/src/app/user/components/the-user-artist/the-user-artist.component.ts
+++ frontend/src/app/user/components/the-user-artist/the-user-artist.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MatCard, MatCardContent, MatCardImage, MatCardTitle} from "@angular/material/card";
 import {MatGridList, MatGridTile} from "@angular/material/grid-list";
+import {NgForOf} from "@angular/common";
+import {User} from "../../../shared/model/user.entity";
+import {UsersService} from "../../../shared/service/users.service";
 
 @Component({
   selector: 'app-the-user-artist',
@@ -11,11 +14,20 @@ import {MatGridList, MatGridTile} from "@angular/material/grid-list";
     MatCardContent,
     MatCardImage,
     MatGridList,
-    MatGridTile
+    MatGridTile,
+    NgForOf
   ],
   templateUrl: './the-user-artist.component.html',
   styleUrl: './the-user-artist.component.css'
 })
-export class TheUserArtistComponent {
+export class TheUserArtistComponent implements OnInit{
+  Artists: User[] = [];
+  constructor(private artistsService: UsersService) {
+  }
 
+  ngOnInit(): void {
+    this.artistsService.getAll().subscribe((data: User[]) => {
+      this.Artists = data;
+    });
+  }
 }
