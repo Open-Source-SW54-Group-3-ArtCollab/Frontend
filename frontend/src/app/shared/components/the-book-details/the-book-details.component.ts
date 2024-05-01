3d3379ea1882bb9d4f4f4d65d7a7ce433df4ea34
Samira Alvarez <samirajetzabel@gmail.com>
2024-05-01 12:01:33 -0500

    feat(the-book-details): connect with fake API

--- frontend/src/app/shared/components/the-book-details/the-book-details.component.ts
+++ frontend/src/app/shared/components/the-book-details/the-book-details.component.ts
@@ -4,6 +4,8 @@ import {faEye, faHeart} from "@fortawesome/free-solid-svg-icons";
 import {FaIconComponent} from "@fortawesome/angular-fontawesome";
 import {MatButton} from "@angular/material/button";
 import {NgForOf, NgOptimizedImage} from "@angular/common";
+import {Book} from "../../../content/model/book.entity";
+import {BookService} from "../../../content/service/book.service";
 
 @Component({
   selector: 'app-the-book-details',
@@ -21,18 +23,25 @@ export class TheBookDetailsComponent {
   protected readonly faHeart = faHeart;
   protected readonly faEye = faEye;
 
-  books=[
-    {cover:'https://m.media-amazon.com/images/I/81mUJVc1MkL._AC_UF1000,1000_QL80_DpWeblab_.jpg',
-      title:'Boulevard',
-      authors:'Flor M. Salvador / MMIvens',
-      date_publish:'Actualizada abr. 02, 2024 10:11 AM',
-      views:'33 M',
-      likes:'15 M',},
-    {cover:'https://m.media-amazon.com/images/I/81mUJVc1MkL._AC_UF1000,1000_QL80_DpWeblab_.jpg',
-      title:'Boulevard',
-      authors:'Flor M. Salvador / MMIvens',
-      date_publish:'Actualizada abr. 02, 2024 10:11 AM',
-      views:'33 M',
-      likes:'15 M',},
-  ]
+  books:Book[] = [];
+
+  constructor(private bookService: BookService) {
+  }
+
+  ngOnInit(): void {
+    this.getBooks();
+  }
+
+  getBooks() {
+    this.bookService.getAll().subscribe((data: any) => {
+      data.forEach((template: any) => {
+        if (template.type === 'book') {
+          const bookData = new Book(template.title, template.description, template.date_publish, template.type, template.id, template.imgUrl, template.likes, template.views);
+          this.books.push(bookData);
+        }
+      });
+    });
+    this.books = this.books.slice(0,3);
+  }
+
 }
