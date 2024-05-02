8bf8b8dbd1d176c6fabb233bb76afade5a27974f
CamiAm404 <u202114248@upc.edu.pe>
2024-05-01 19:55:35 -0500

    feat(details-edit-cover): Added new method
    Added method to change cover image

--- frontend/src/app/content/components/the-book-details-edit/the-book-details-edit-cover/the-book-details-edit-cover.component.ts
+++ frontend/src/app/content/components/the-book-details-edit/the-book-details-edit-cover/the-book-details-edit-cover.component.ts
@@ -29,4 +29,10 @@ export class TheBookDetailsEditCoverComponent implements OnInit {
          });
      });
   }
+  editImage() {
+    const newImageUrl = prompt('Por favor, ingresa la nueva URL de la imagen');
+    if (newImageUrl) {
+      this.bookData.imgUrl = newImageUrl;
+    }
+  }
 }
