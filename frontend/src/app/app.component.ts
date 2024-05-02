eae9808c4d81c5cd2cb7f77f6322ef940a584735
Mathias Jave Diaz <u202212721@upc.edu.pe>
2024-05-02 02:05:39 -0500

    feat(collaboration): added chat section feature

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import {NavbarContentComponent} from "./public/components/navbar-content/navbar-content.component";
 import {FooterContentComponent} from "./public/components/footer-content/footer-content.component";
 import {ArtistRoomComponent} from "./collaboration/pages/the-artist-room/artist-room.component";
+import {TheChatPageComponent} from "./collaboration/pages/the-chat-page/the-chat-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarContentComponent, FooterContentComponent, ArtistRoomComponent],
+  imports: [RouterOutlet, NavbarContentComponent, FooterContentComponent, ArtistRoomComponent, TheChatPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ frontend/src/app/collaboration/service/chat.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatService {
+
+  constructor(private http: HttpClient) { }
+  createChatRoom(data: any) {
+      return this.http.post('http://localhost:3000/chatRoom', data);
+  }
+  getChatRoomById(id: string) {
+    return this.http.get('http://localhost:3000/chatRoom/' + id);
+  }
+  getChats() {
+       return this.http.get('http://localhost:3000/chat');
+  }
+  sendChatMessage(data: any) {
+      return this.http.post('http://localhost:3000/message', data);
+  }
+  getMessages() {
+      return this.http.get('http://localhost:3000/message/');
+  }
+}
