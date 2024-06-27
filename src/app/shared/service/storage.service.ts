c5a333b06a6f4ef9fc617e6fa9528e27afa073e7
GustavoHC <u202213983@upc.edu.pe>
2024-06-27 13:42:53 -0500

    fix: localstorage

--- src/app/shared/service/storage.service.ts
+++ src/app/shared/service/storage.service.ts
@@ -1,35 +1,39 @@
-import {Injectable} from "@angular/core";
+import {inject, Injectable, PLATFORM_ID} from "@angular/core";
+import {isPlatformBrowser} from "@angular/common";
 
 @Injectable({
   providedIn: 'root'
 })
 export class StorageService {
-  private isBrowser: boolean;
+  private readonly platformId= inject(PLATFORM_ID)
 
   constructor() {
-    this.isBrowser = typeof window !== 'undefined';
+
   }
 
   setItem(key: string, value: string): void {
-    if (this.isBrowser) {
+    if (isPlatformBrowser(this.platformId)) {
       localStorage.setItem(key, value);
-    } else {
+    }
+    else {
       return;
     }
   }
 
   getItem(key: string): string | null {
-    if (this.isBrowser) {
+    if (isPlatformBrowser(this.platformId)) {
       return localStorage.getItem(key);
-    } else {
-      return null;
+    }
+    else {
+      return '';
     }
   }
 
   removeItem(key: string): void {
-    if (this.isBrowser) {
+    if (isPlatformBrowser(this.platformId)) {
       localStorage.removeItem(key);
-    } else {
+    }
+    else {
       return;
     }
   }
